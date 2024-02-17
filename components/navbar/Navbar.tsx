af3b2effd52d896e83c6f9194f99a5100d2841d9
imran khan <imranloveh01@gmail.com>
2024-02-18 01:48:18 +0500

    Add Navbar component to layout

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Container from '../Container'
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+       <div className="border-b-[1px] py-4">
+            <Container>
+                sfa
+            </Container>
+       </div>
+        </div>
+  )
+}
+
+export default Navbar
