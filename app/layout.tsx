c69f989a1aae1f721d76fa956258f3826fcfe726
imran <imranloveh01@gmail.com>
2024-02-21 15:59:29 +0500

    Update Modal component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal  />
+        <Modal isOpen />
         <Navbar />
         {children}
         </body>
--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -71,7 +71,14 @@ const Modal: FC<ModalProps> = ({
       <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
         <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
             {/* content */}
-            
+            <div
+            className={`
+                translate duration-300 h-full ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                ${showModal ? 'opacity-100' : 'opacity-0'}
+            `}
+            >
+
+            </div>
         </div>
       </div>
     </>
