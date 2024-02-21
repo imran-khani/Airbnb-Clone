751fc03ba7a5e5cf1dbfcc9f045d410fb112e5a9
imran <imranloveh01@gmail.com>
2024-02-21 23:06:11 +0500

    Refactor Input and RegisterModal components

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,6 +1,10 @@
-"use client";
+'use client';
 
-import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { 
+  FieldErrors, 
+  FieldValues, 
+  UseFormRegister 
+} from "react-hook-form";
 import { BiDollar } from "react-icons/bi";
 
 interface InputProps {
@@ -10,37 +14,79 @@ interface InputProps {
   disabled?: boolean;
   formatPrice?: boolean;
   required?: boolean;
-  register: UseFormRegister<FieldValues>;
-  errors: FieldErrors;
+  register: UseFormRegister<FieldValues>,
+  errors: FieldErrors
 }
 
 const Input: React.FC<InputProps> = ({
   id,
   label,
-  type = "text",
-  disabled,
+  type = "text", 
+  disabled, 
   formatPrice,
-  required,
   register,
+  required,
   errors,
 }) => {
   return (
     <div className="w-full relative">
       {formatPrice && (
         <BiDollar
-          size={24}
-          className="text-neutral-700 absolute top-5 left-2"
+          size={24}  
+          className="
+            text-neutral-700
+            absolute
+            top-5
+            left-2
+          "
         />
       )}
-
       <input
-      id={id}
-      disabled={disabled}
-      placeholder=""
-      {...register(id,{required})}
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=" "
+        type={type}
+        className={`
+          peer
+          w-full
+          p-4
+          pt-6 
+          font-light 
+          bg-white 
+          border-2
+          rounded-md
+          outline-none
+          transition
+          disabled:opacity-70
+          disabled:cursor-not-allowed
+          ${formatPrice ? 'pl-9' : 'pl-4'}
+          ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
+          ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
+        `}
       />
+      <label 
+        className={`
+          absolute 
+          text-md
+          duration-150 
+          transform 
+          -translate-y-3 
+          top-5 
+          z-10 
+          origin-[0] 
+          ${formatPrice ? 'left-9' : 'left-4'}
+          peer-placeholder-shown:scale-100 
+          peer-placeholder-shown:translate-y-0 
+          peer-focus:scale-75
+          peer-focus:-translate-y-4
+          ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
+        `}
+      >
+        {label}
+      </label>
     </div>
-  );
-};
-
+   );
+}
+ 
 export default Input;
--- components/modal/RegisterModal.tsx
+++ components/modal/RegisterModal.tsx
@@ -1,78 +1,144 @@
-'use client'
-import axios from "axios"
-import { AiFillGithub } from "react-icons/ai"
-import { FcGoogle } from "react-icons/fc"
-import { useCallback,useState } from "react"
-import toast from "react-hot-toast"
-import { FieldValues,SubmitHandler,useForm } from "react-hook-form"
-import { Modal,Input,Heading,Button } from ".."
-import useRegisterModal from "@/hooks/useResgisterModal"
-import useLoginModal from "@/hooks/useLoginModal"
+'use client';
 
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+import { 
+  FieldValues, 
+  SubmitHandler,
+  useForm
+} from "react-hook-form";
 
-const RegisterModal = () => {
+import useLoginModal from "@/hooks/useLoginModal"; 
+import useRegisterModal from "@/hooks/useResgisterModal";
+import {Modal,Heading,Button,Input} from '..'
 
- const [isLoading,setIsLoading] = useState<boolean>(false)
-    const registerModal = useRegisterModal()
-    const loginModal = useLoginModal()
-const {register,handleSubmit,formState:{errors}} = useForm({
-    defaultValues:{
-        name:'',
-        email:'',
-        password:'',
-    }
-})
+const RegisterModal= () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-const onSubmit:SubmitHandler<FieldValues> = (data) =>{
-        setIsLoading(true)
+  const { 
+    register, 
+    handleSubmit,
+    formState: {
+      errors,
+    },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+  });
 
-        axios.post('/api/register',data)
-        .then(()=>{
-            toast.success('Registerd!')
-            registerModal.onClose();
-            loginModal.onOpen();
-        }).catch((error)=>{
-            toast.error(error)
-        }).finally(()=>{
-            setIsLoading(false)
-        })}
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
 
-        const onToggle =useCallback(()=>{
-                registerModal.onClose()
-                loginModal.onOpen()
-        },[registerModal,loginModal])
+    axios.post('/api/register', data)
+    .then(() => {
+      toast.success('Registered!');
+      registerModal.onClose();
+      loginModal.onOpen();
+    })
+    .catch((error) => {
+      toast.error(error);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    })
+  }
 
-        const bodyContent = (
-            <div className="flex flex-col gap-4">
-            <Heading
-            title="Welcome to Airbnb"
-            subtitle="Create an account!"
-          />
-          <Input  
-            id="name"
-            label="Name"
-          />
-          </div>
-        )
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal])
 
-        const footerContent = (
-            <div></div>
-        )
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Welcome to Airbnb"
+        subtitle="Create an account!"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button 
+        outline 
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}} 
+      />
+      <Button 
+        outline 
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div 
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>Already have an account?
+          <span 
+            onClick={onToggle} 
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+            > Log in</span>
+        </p>
+      </div>
+    </div>
+  )
 
   return (
     <Modal
-     actionLabel="Continue"
-     onSubmit={handleSubmit(onSubmit)}
-     disabled={isLoading}
-     isOpen={registerModal.isOpen}
-     onClose={registerModal.onClose}
-     title="Register"
-     body={bodyContent}
-     footer={footerContent}
-
-    
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
     />
-  )
+  );
 }
 
-export default RegisterModal
+export default RegisterModal;
