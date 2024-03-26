cdb5cf5d07d867f67bded6408bcd73c282fa1d6e
imran khan <imranloveh01@gmail.com>
2024-03-26 21:30:51 +0200

    Add Login and Register Modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import {Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/app/components/navbar/Navbar'
+import LoginModal from "./components/modals/LoginModal";
+import RegisterModal from "./components/modals/RegisterModal";
 const nunito =Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <LoginModal />
+        <RegisterModal />
         <Navbar />
         {children}
         </body>
