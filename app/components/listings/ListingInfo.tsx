e91fd0419c4894da8ddf9d3edc4834748044d53f
imran khan <imranloveh01@gmail.com>
2024-04-17 22:56:50 +0500

    Refactor Leaflet dependencies and update Map component

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -3,6 +3,11 @@
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import { IconType } from "react-icons";
+import Avatar from "../Avatar";
+import ListingCategory from "./ListingCategory";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../Map"), { ssr: false });
 
 interface ListingInfoProps {
     user: SafeUser;
@@ -38,10 +43,48 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                 <div className="text-xl font-semibold flex flex-row items-center gap-2">
                     <div>
                         Hosted by{" "}
-                        <span className="text-gray-500">{user?.name}</span>
+                        <span className="text-gray-500 inline-block">
+                            {user?.name}
+                        </span>
+                    </div>
+                    <Avatar src={user.image} />
+                </div>
+                <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
+                    <div>
+                        <span className="text-xl font-semibold">
+                            {roomCount}
+                        </span>{" "}
+                        rooms
+                    </div>
+                    <div>
+                        <span className="text-xl font-semibold">
+                            {guestCount}
+                        </span>{" "}
+                        guests
+                    </div>
+                    <div>
+                        <span className="text-xl font-semibold">
+                            {bathroomCount}
+                        </span>{" "}
+                        bathrooms
                     </div>
                 </div>
             </div>
+            <hr />
+            {category && (
+                <ListingCategory
+                    icon={category.icon}
+                    label={category.label}
+                    description={category.description}
+                />
+            )}
+            <hr />
+
+            <div className="text-lg font-light text-neutral-500">
+                {description}
+            </div>
+            <hr />
+            {/* <Map center={coordinates} /> */}
         </div>
     );
 };
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,54 +1,77 @@
-'use client'
+"use client";
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
 import ListingInfo from "@/app/components/listings/ListingInfo";
 import { categories } from "@/app/components/navbar/Categories";
+import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
+import { eachDayOfInterval } from "date-fns";
+import { useRouter } from "next/navigation";
 import { useMemo } from "react";
 
 interface ListingClientProps {
     reservations?: Reservation[];
-    listing:SafeListing & {
-        user:SafeUser;
-    }
-    currentUser?:SafeUser | null;
+    listing: SafeListing & {
+        user: SafeUser;
+    };
+    currentUser?: SafeUser | null;
 }
 
-const ListingClient:React.FC<ListingClientProps> = ({
+const initialDateRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+};
+
+const ListingClient: React.FC<ListingClientProps> = ({
     listing,
-    currentUser
+    currentUser,
+    reservations = [],
 }) => {
-    const category = useMemo(()=>{
-        return categories.find((item)=>item.label=== listing.category)
-    },[listing.category]) 
-  return (
-   <Container>
-    <div className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-6">
-            <ListingHead 
-            title={listing.title}
-            imageSrc={listing.imageSrc}
-            locationValue={listing.locationValue}
-            id = {listing.id}
-            currentUser={currentUser}
-            />
-            <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
-                <ListingInfo 
-                user={listing.user}
-                category={category}
-                description={listing.description}
-                roomCount={listing.roomCount}
-                guestCount={listing.guestCount}
-                bathroomCount={listing.bathroomCount}
-                locationValue={listing.locationValue}
-                
-                />
-            </div>
-        </div>
-    </div>
-   </Container>
-  )
-}
+    const loginModal = useLoginModal();
+    const router = useRouter();
 
-export default ListingClient
+    const disabledDates = useMemo(() => {
+        let dates: Date[] = [];
+
+        reservations.forEach((reservation) => {
+            const range = eachDayOfInterval({
+                start: new Date(reservation.startDate),
+                end: new Date(reservation.endDate),
+            });
+        });
+    }, []);
+
+    const category = useMemo(() => {
+        return categories.find((item) => item.label === listing.category);
+    }, [listing.category]);
+    return (
+        <Container>
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col gap-6">
+                    <ListingHead
+                        title={listing.title}
+                        imageSrc={listing.imageSrc}
+                        locationValue={listing.locationValue}
+                        id={listing.id}
+                        currentUser={currentUser}
+                    />
+                    <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
+                        <ListingInfo
+                            user={listing.user}
+                            category={category}
+                            description={listing.description}
+                            roomCount={listing.roomCount}
+                            guestCount={listing.guestCount}
+                            bathroomCount={listing.bathroomCount}
+                            locationValue={listing.locationValue}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
+};
+
+export default ListingClient;
