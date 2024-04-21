c3a2afc4168eb02c3dc5805b6c584d9f045273f9
imran khan <imranloveh01@gmail.com>
2024-04-21 18:49:23 +0500

    Refactor getListings function to accept optional parameters and improve query logic in getListings.ts

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,32 +1,93 @@
 import prisma from '@/app/lib/prismadb'
 
-export interface IListingParams{
-    userId?:string
+export interface IListingParams {
+    userId?: string;
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    categorey?: string;
 }
 
 export async function getListings(
-    params:IListingParams
+    params: IListingParams
 ) {
     try {
-        const {userId} = params
-        const query:any ={}
-        if(userId){
-            query.userId = userId 
+        const {
+            userId,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            startDate,
+            endDate,
+            locationValue,
+            categorey
+        } = params
+
+
+        const query: any = {}
+        if (userId) {
+            query.userId = userId
+        }
+        if (categorey) {
+            query.categorey = categorey;
+        }
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
         }
 
-        
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+
+        if (guestCount) {
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue;
+        }
+
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservations: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: { gte: startDate },
+                                startDate: { lte: startDate }
+                            },
+                            {
+                                endDate: { gte: endDate },
+                                startDate: { lte: endDate }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
         const listings = await prisma.listing.findMany({
-            where:query,
+            where: query,
             orderBy: {
                 createdAt: 'desc'
             }
         })
-        const safeListing = listings.map((listing)=>({
+        const safeListing = listings.map((listing) => ({
             ...listing,
-            createdAt:listing.createdAt.toISOString(),
+            createdAt: listing.createdAt.toISOString(),
         }))
 
-        return safeListing 
+        return safeListing
     } catch (error: any) {
         throw new Error(error);
     }
