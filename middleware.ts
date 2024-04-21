a162a10840eefd636fba51a39ac47b27b34b41dc
imran khan <imranloveh01@gmail.com>
2024-04-21 19:53:55 +0500

    Fix formatting in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-export {default} from 'next-auth/middleware'
+export { default } from 'next-auth/middleware'
 
 export const config = {
-    matcher:[
+    matcher: [
         '/trips',
         '/reservations',
         '/properties',
